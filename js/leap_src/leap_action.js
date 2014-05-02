659502a629bbd0542f4c18b32c3cd2948f42dfa1
CW. LAU <contact@cwlau.com>
2014-05-02 12:47:56 +0800

    hello

---
+++ js/leap_src/leap_action.js
@@ -0,0 +1,103 @@
+/*
+ * List of actions defined for production
+ * 
+ * Please refer to ./leap_src/leap_action_demo.js 
+ *    and ./action.html for the usage example
+ *
+ */
+
+ 
+var action = {
+  
+  // Leave blank for undefined actions
+  zeroFinger : function(){
+  
+  
+  },
+  
+  fiveFinger : function(){
+  
+  
+  },
+  
+  swipeLeft: function(){
+	/*
+	var divType = $(".leapPointed").data("type");
+	
+	var selector;
+	
+	if(divType =="product"){
+		selector = ".item";
+	}else if(divType == "productList"){
+		selector = ".productListItem";
+	}else if(divType == "mainMenu"){
+		selector = ".mainMenuContainer";
+	}else{
+		//
+	}
+	
+	var selectedItem = $(".leapPointed");
+
+	var selectedIndex = $(selector).index(selectedItem);
+
+	var toBeSelectedIndex = selectedIndex - 1 > 0 ? selectedIndex - 1 :	 0;
+
+	var toBeSelectedItem = $(selector).get(toBeSelectedIndex);
+	
+	selectedItem.removeClass("leapPointed").data("pointing", "");
+	
+	$(toBeSelectedItem).addClass("leapPointed").data("pointing", "true");
+
+	leapUI.selectItem();
+	
+	*/
+	
+	leapUI.navNextProductItemPage(-1);
+  },
+  
+  swipeRight: function(){
+	/*
+	var divType = $(".leapPointed").data("type");
+		
+	var selector;
+	
+	if(divType =="product"){
+		selector = ".item";
+	}else if(divType == "productList"){
+		selector = ".productListItem";
+	}else if(divType == "mainMenu"){
+		selector = ".mainMenuContainer";
+	}else{
+		//
+	}
+	
+	var selectedItem = $(".leapPointed");
+
+	var selectedIndex = $(selector).index(selectedItem);
+
+	var toBeSelectedIndex = selectedIndex + 1 == $(selector).length ? $(selector).length-1 : selectedIndex + 1;
+
+	var toBeSelectedItem = $(selector).get(toBeSelectedIndex);
+	
+	selectedItem.removeClass("leapPointed").data("pointing", "");
+	
+	$(toBeSelectedItem).addClass("leapPointed").data("pointing", "true");
+
+	leapUI.selectItem();	
+	*/
+	leapUI.navNextProductItemPage(1);
+	
+  },
+  
+  pointed: function(){
+    /* 
+     * Applied to all the $("div.pointable") objects
+     *
+     * The following statement wont trigger click for <a href=""> element.
+     * Instead, this only applies to all elements with "onclick" attribute
+     */
+    $(".leapHover").trigger("click");
+	
+  }
+
+};
---
+++ js/loader.js
@@ -0,0 +1,156 @@
+/*
+	items
+		itemPage
+			item
+				<item detail>.
+
+
+*/
+var itemToDisplay = 4;
+
+function load(){
+	
+	if($("#itemsContainer").length!=0){
+		loadItem();
+		uiRender.updateItemSize();
+		uiRender.updateItemImageSize();
+		
+		//uiRender.itemAddMouseover();
+		
+		window.onresize = function(){
+			uiRender.updateItemSize();
+			uiRender.updateItemImageSize();
+		}
+		
+		uiRender.addArrowNav();
+		uiRender.itemAddMouseClick();
+	}else if($("#orderHistoryAccordion").length!=0){
+		var icons = {
+		  header: "ui-icon-circle-arrow-e",
+		  activeHeader: "ui-icon-circle-arrow-s"
+		};
+		$( "#orderHistoryAccordion" ).accordion({
+		  icons: icons
+		});
+		
+		$(".ui-accordion-header").each(function(i,v){
+			$(v).addClass("pointable");
+		});
+	}
+	
+	uiRender.addBackButtonEvent();
+	
+	
+}
+
+function loadItem(){
+
+	var request = new XMLHttpRequest();
+	
+	request.onload = function() {
+		var xml = new DOMParser().parseFromString(request.responseText,'text/xml');
+		var items = xml.getElementsByTagName("item");
+		
+		var itemsDiv = document.createElement('div');
+		itemsDiv.className = "items";
+		document.getElementById("itemsContainer").appendChild(itemsDiv);
+		
+		for(var a = 0 ; a < Math.ceil(items.length/itemToDisplay);a++){
+			
+			var pageDiv = document.createElement('div');
+			pageDiv.id="itemPage_"+(a+1);
+			if(a!=0){
+				pageDiv.className="hiddenItem";
+			}
+			
+			
+			
+			
+			for(var i = a*itemToDisplay; i < a*itemToDisplay + itemToDisplay; i++) {
+			
+				var item = items[i];
+				
+				if(item != null){
+					var name = item.getElementsByTagName("name")[0].childNodes[0].nodeValue;
+					var desc = item.getElementsByTagName("desc")[0].childNodes[0].nodeValue;
+					var imageName = item.getElementsByTagName("imageName")[0].childNodes[0].nodeValue;
+					var price = item.getElementsByTagName("price")[0].childNodes[0].nodeValue;
+					var rate =  item.getElementsByTagName("rate")[0].childNodes[0].nodeValue;
+					
+					
+					
+					var itemDiv = document.createElement('div');
+					itemDiv.id = "item_" + (i+1);
+					itemDiv.setAttribute("data-type", "product");
+					
+					//$(itemDiv).data("type", "product");
+					itemDiv.className = "item includeShadow pointable ";
+					
+					
+					var itemLeftDiv = document.createElement('div');
+					itemLeftDiv.className = "itemLeftDiv";
+					var itemRightDiv = document.createElement('div');
+					itemRightDiv.className = "itemRightDiv";
+					itemDiv.appendChild(itemLeftDiv);
+					itemDiv.appendChild(itemRightDiv);
+					
+					
+					var imgDiv = document.createElement('div');
+					imgDiv.className= "itemImgContainer";
+					
+					var img = document.createElement('img');
+					img.src = "shopItemImage/" + imageName;
+					img.className = "itemImg";
+					
+					
+				
+					imgDiv.appendChild(img);
+					
+					itemLeftDiv.appendChild(imgDiv);
+					
+					var nameDiv = document.createElement('div');
+					nameDiv.innerHTML = name;
+					nameDiv.className = "name";
+					itemLeftDiv.appendChild(nameDiv);
+	
+					var rateAndPriceDiv = document.createElement('div');
+					rateAndPriceDiv.className = "rateAndPriceDiv";
+					
+					var priceDiv = document.createElement('div');
+					priceDiv.innerHTML = "$" + price;
+					priceDiv.className = "price";
+					rateAndPriceDiv.appendChild(priceDiv);
+					
+					var rateDiv = document.createElement('div');
+					rateDiv.innerHTML = "Rating: " + rate + "/ 5.0";
+					rateDiv.className = "rate";
+					rateAndPriceDiv.appendChild(rateDiv);	
+
+					itemRightDiv.appendChild(rateAndPriceDiv);
+
+	
+					var descDiv = document.createElement('div');
+					descDiv.innerHTML = desc;
+					descDiv.className = "desc";
+					itemRightDiv.appendChild(descDiv);
+					
+					
+				
+					pageDiv.appendChild(itemDiv);
+					
+					
+					
+				}
+				
+			}
+			
+			itemsDiv.appendChild(pageDiv);
+			
+		}
+	}
+	
+	request.open("GET","./xml/" + xmlInfoFile, false);
+    request.send();
+
+
+}
